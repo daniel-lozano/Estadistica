ec54dc390ca5123e322289834a744e2a9b62d165
Daniel Lozano <d.lozano343@uniandes.edu.co>
2014-08-13 22:35:59 -0500

    avance

---
+++ puntos.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+int  main (){
+
+  double *r;
+  int N=pow(10,5);
+  int M=100;
+  double dim= rand();
+  int i,j=0;
+  FILE *f;
+
+  r=malloc(sizeof(float)*N);
+  f=fopen("datos.dat","w");
+ 
+
+  //  srand(time(NULL));
+
+
+
+  //random walk------------------
+
+  for(i=0;i<=N;i++){
+    //haciendo un punto
+    int x,y=0;
+
+    for(j=0;j<=M;j++){
+    
+      float dim= (rand()+0.0)/RAND_MAX;
+      float dir= (rand()+0.0)/RAND_MAX;
+      //toma valores en x----------------------------
+      if(dim<=0.5){
+	
+	if(dir<=0.5){
+	  x=x+1;
+	}
+	if(dir>0.5){
+	  x=x-1;
+	}
+      }
+      //toma valores en y----------------------------
+      if(dim>0.5){
+	
+	if(dir<=0.5){
+	  y=y+1;
+	}
+	if(dir>0.5){
+	  y=y-1;
+	}
+      }
+}
+    double radio= pow(x*x +y*y,0.5);
+    fprintf(f,"%lf\n",radio);
+    
+}
+  printf("numero de puntos %i numero de pasos %i \n",N,M);
+ 
+  // double random=srand48(void);
+  //printf("numero de srand 48  %lf \n",random);
+
+
+  return 0;
+
+}
